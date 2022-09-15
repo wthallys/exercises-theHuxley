4a22021d6ef61f21e25abbfa3332057e8aeed295
wthallys <wthallys.02@gmail.com>
2022-09-15 02:19:54 -0300

    vaiOriginal.py added and vaiPraOnde.py modified

--- lista2_Semestre2/vaiOriginial.py
+++ lista2_Semestre2/vaiOriginial.py
@@ -2,24 +2,18 @@ grana = 300
 city = "SEM DESTINO"
 km = 0
 while True:
-    try:
-        city_temp = input()
-    except EOFError:
-        break
+    city_temp = input()
     if city_temp == "FIM":
         break
     elif city_temp == "":
         print("SEM DESTINO")
         break
     else:
-        try:
-            km_temp = int(input())
-        except EOFError:
-            break
+        km_temp = int(input())
         passagem = 2 * float(input())
         if passagem <= 300:
             if km_temp > km:
                 km = km_temp
                 city = city_temp
-    
+
 print(city.upper())
